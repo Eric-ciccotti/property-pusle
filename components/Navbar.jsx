2eff998a1463e38d90d5ef8c058ce5347743ee81
Eric <eciccotti.aubay@lfdj.com>
2024-04-07 15:45:20 +0200

    add react icons fa

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import logo from '@/assets/images/logo-white.png'
 import profileDefault from '@/assets/images/profile.png';
+import Link from "next/link";
+import {FaGoogle} from "react-icons/fa";
 
 const Navbar = () => {
 return(
@@ -36,7 +38,7 @@ return(
       </div>
       <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
         {/* Logo */}
-        <a className="flex flex-shrink-0 items-center" href="/index.html">
+        <Link className="flex flex-shrink-0 items-center" href="/">
           <Image
             className="h-10 w-auto"
             src={logo}
@@ -45,28 +47,28 @@ return(
           <span className="hidden md:block text-white text-2xl font-bold ml-2">
             PropertyPulse
           </span>
-        </a>
+        </Link>
         {/* Desktop Menu Hidden below md screens */}
         <div className="hidden md:ml-6 md:block">
           <div className="flex space-x-2">
-            <a
-              href="/index.html"
+            <Link
+              href="/"
               className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
             >
               Home
-            </a>
-            <a
-              href="/properties.html"
+            </Link>
+            <Link
+              href="/properties"
               className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
             >
               Properties
-            </a>
-            <a
-              href="/add-property.html"
+            </Link>
+            <Link
+              href="/properties/add"
               className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
             >
               Add Property
-            </a>
+            </Link>
           </div>
         </div>
       </div>
@@ -74,14 +76,14 @@ return(
       <div className="hidden md:block md:ml-6">
         <div className="flex items-center">
           <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-            <i className="fa-brands fa-google text-white mr-2" />
+           <FaGoogle className="text-white mr-2" />
             <span>Login or Register</span>
           </button>
         </div>
       </div>
       {/* Right Side Menu (Logged In) */}
       <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-        <a href="messages.html" className="relative group">
+        <Link href="/messages" className="relative group">
           <button
             type="button"
             className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -106,7 +108,7 @@ return(
           <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
             2{/* Replace with the actual number of notifications */}
           </span>
-        </a>
+        </Link>
         {/* Profile dropdown button */}
         <div className="relative ml-3">
           <div>
@@ -135,33 +137,32 @@ return(
             aria-labelledby="user-menu-button"
             tabIndex={-1}
           >
-            <a
-              href="/profile.html"
+            <Link
+              href="/profile"
               className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
               tabIndex={-1}
               id="user-menu-item-0"
             >
               Your Profile
-            </a>
-            <a
-              href="saved-properties.html"
+            </Link>
+            <Link
+              href="/properties/saved"
               className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
               tabIndex={-1}
               id="user-menu-item-2"
             >
               Saved Properties
-            </a>
-            <a
-              href="#"
+            </Link>
+            <button
               className="block px-4 py-2 text-sm text-gray-700"
               role="menuitem"
               tabIndex={-1}
               id="user-menu-item-2"
             >
               Sign Out
-            </a>
+            </button>
           </div>
         </div>
       </div>
@@ -170,24 +171,24 @@ return(
   {/* Mobile menu, show/hide based on menu state. */}
   <div className="hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pb-3 pt-2">
-      <a
-        href="/index.html"
+      <Link
+        href="/"
         className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
       >
         Home
-      </a>
-      <a
-        href="/properties.html"
+      </Link>
+      <Link
+        href="/properties"
         className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
       >
         Properties
-      </a>
-      <a
-        href="/add-property.html"
+      </Link>
+      <Link
+        href="/propreties/add"
         className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
       >
         Add Property
-      </a>
+      </Link>
       <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
         <i className="fa-brands fa-google mr-2" />
         <span>Login or Register</span>
