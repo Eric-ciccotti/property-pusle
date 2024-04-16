2373546026adc24573c00e77d5ebcce6a1513299
Eric <eciccotti.aubay@lfdj.com>
2024-04-17 00:45:17 +0200

    add loading component and properties.json

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
-import Link from 'next/link';
 
 const HomePage = () => {
   return (
