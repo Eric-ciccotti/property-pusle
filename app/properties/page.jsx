927c8d90dc444ca588336d0af1d4b71cbb9e2b1c
Eric <eciccotti.aubay@lfdj.com>
2024-04-20 13:42:20 +0200

    fetch properties from mongo, add fetch method for properties

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,21 +1,35 @@
-import properties from '@/properties.json'
+// import properties from '@/properties.json'
 import PropertyCard from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json()
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+  properties.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
   return (
     <section className="px-4 py-6">
-    <div className="container-xl lg:container m-auto px-4 py-6">
-      {!properties.length ? (
-        <p>No properties found !</p>
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.map((property) => (
-         < PropertyCard key={property._id} property={property}/>
-        ))}
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {!properties?.length ? (
+          <p>No properties found !</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
       </div>
-      )}
-
-    </div>
     </section>
   );
 };
