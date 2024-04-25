1456279c958a526034f8121ecefafa210a72e96c
Eric <eciccotti.aubay@lfdj.com>
2024-04-25 07:58:10 +0200

    add server component and property header

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,17 +1,5 @@
-// import properties from '@/properties.json'
 import PropertyCard from '@/components/PropertyCard';
-
-async function fetchProperties() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    return res.json()
-  } catch (error) {
-    console.log(error);
-  }
-}
+import { fetchProperties } from '@/utils/request';
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
