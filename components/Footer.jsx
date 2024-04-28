e6dc4f8b38435d4dec4179a57ca3dd494b1c4cc5
Eric <eciccotti.aubay@lfdj.com>
2024-04-28 14:39:11 +0200

    add property details components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-gray-200 py-4 mt-30">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
-          <Image src={logo} alt="Logo" className="h-8 w-auto" />
+          <Image src={logo} alt="Logo" className="h-8 w-auto" priority={true}/>
         </div>
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
