b996fea6b5cd863115a74ecc7a88bd71d263e0fc
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2024-05-03 03:03:13 +0200

    final fix at 2:57 pm

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,6 +1,8 @@
 import connectDB from '@/config/database';
 import User from '@/models/User';
+
 import GoogleProvider from 'next-auth/providers/google';
+
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -8,41 +10,42 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
   callbacks: {
-    //invocked in sucessfull login
+    // Invoked on successful signin
     async signIn({ profile }) {
-      //1. connect to db
+      // 1. Connect to database
       await connectDB();
-
-      //2. check if user exists
+      // 2. Check if user exists
       const userExists = await User.findOne({ email: profile.email });
-
-      //3. if not add user to db
+      // 3. If not, then add user to database
       if (!userExists) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
         await User.create({
           email: profile.email,
-          username: profile.username.slice(0, 20),
-          image: profile.picture
-        })
+          username,
+          image: profile.picture,
+        });
       }
-      //4. return true to allowed signin
-      return true
+      // 4. Return true to allow sign in
+      return true;
     },
-    //Modifies the session object
+    // Modifies the session object
     async session({ session }) {
-      //1. get user from db
+      // 1. Get user from database
       const user = await User.findOne({ email: session.user.email });
-      //2. assign the user id to the session
+      // 2. Assign the user id to the session
       session.user.id = user._id.toString();
-      //3. return session
+      // 3. return session
       return session;
-    }
+    },
   },
 };
